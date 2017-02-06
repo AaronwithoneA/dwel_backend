99d73eb98f8983128a7b59e6687c5c6af2df74d3
Victor Guillen <victorguillen@Victors-MacBook-Air.local>
2017-02-05 18:49:46 -0800

    addedcol

--- db/migrate/20170206023815_change_col_comment_table.rb
+++ db/migrate/20170206023815_change_col_comment_table.rb
@@ -1,7 +1,6 @@
 class ChangeColCommentTable < ActiveRecord::Migration[5.0]
   def change
-    remove_column :comments, :user_id
-    add_column :comments, :username, :string, null: false
+
 
   end
 end
---
+++ db/migrate/20170206024823_comments_remove_col.rb
@@ -0,0 +1,5 @@
+class CommentsRemoveCol < ActiveRecord::Migration[5.0]
+  def change
+    
+  end
+end
